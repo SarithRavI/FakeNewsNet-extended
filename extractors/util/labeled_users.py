1b9fae3b8e7d2ccdc87d0d84d16a54ebf0c94d99
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2023-01-08 18:03:24 +0530

    Revert "Getting users as a list."
    This reverts commit 3110bce07f7575480861aa0eac890c17d93e1b2e.

--- extractors/util/labeled_users.py
+++ extractors/util/labeled_users.py
@@ -8,6 +8,6 @@ def getLabeledUsers(dataset, label,init_file_root):
     label_news_user_mask = node_user_news_mapping.isin(label_news_ls)
     node_user_news_mapping['flag'] = label_news_user_mask['news_id']
     label_news_user = node_user_news_mapping[node_user_news_mapping['flag'] == True]
-    label_user_news_groups = list(label_news_user.groupby('news_id', sort=False)['user_id'].apply(set))
+    label_user_news_groups = label_news_user.groupby('news_id', sort=False)['user_id'].apply(set)
     # print(label_user_news_groups)
     return pd.DataFrame(label_user_news_groups)
